8a30732cb2d4fed2882bd6ea7c7220faac6aadec
maTurjo <ma.turjo@gmail.com>
2022-09-30 01:38:12 +0600

    stairs stuck

--- Integer to Roman/index.js
+++ Integer to Roman/index.js
@@ -4,49 +4,21 @@
  */
  var intToRoman = function(num) {
 
-    let numLeft=num;
-    let romanObject=new Object({
-        I:1,
-        V:5,
-        X:10,
-        L:50,
-        C:100,
-        D:500,
-        M:1000
-    });
+     const roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+    const value = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
 
-    let refObject=new Object({
-        I:0,
-        V:0,
-        X:0,
-        L:0,
-        C:0,
-        D:0,
-        M:0
-    })
-    let RomanArray=[];
-
-    refObject.M=parseInt(numLeft/1000);
-    numLeft=numLeft-(1000*refObject.M);
-    refObject.D=parseInt(numLeft/500);
-    numLeft=numLeft-(500*refObject.D);
-    refObject.C=parseInt(numLeft/100);
-    numLeft=numLeft-(100*refObject.C);
-    refObject.L=parseInt(numLeft/50);
-    numLeft=numLeft-(50*refObject.L);
-    refObject.X=parseInt(numLeft/10);
-    numLeft=numLeft-(10*refObject.X);
-    refObject.V=parseInt(numLeft/5);
-    numLeft=numLeft-(5*refObject.V);
-    refObject.I=parseInt(numLeft/1);
-    numLeft=numLeft-(500*refObject.D);
-
-    console.log(refObject)
-
-  
-
+        let ans = ""
+        for (let i = 0; num; i++){
+            console.log(i);
+            while (num >= val[i]){ 
+                ans += rom[i]
+                num -= val[i]
+            }
+        }
+        return ans
+ 
 
 };
 
 
-intToRoman(1994);
+console.log(intToRoman(1994));
